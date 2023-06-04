c89c24ddb203d4cb38c8202f360687d0aeb56966
Rajdeep Barman <deepbn9@gmail.com>
2023-06-04 21:23:36 +0530

    Initial commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            width: 20,
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 237, 223, 252),
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+            height: 80,
+          ),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+        ],
+      ),
+    );
+  }
+}
