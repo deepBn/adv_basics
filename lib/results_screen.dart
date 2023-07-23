0f4634a004325e4a1a85ef275c723b234724bac5
Rajdeep Barman <deepbn9@gmail.com>
2023-07-23 11:06:10 +0530

    Displaying final result on the results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
@@ -34,7 +35,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text("List of answers and questions"),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
